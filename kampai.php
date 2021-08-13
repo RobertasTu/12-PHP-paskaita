262acf2323f2b3837e413282d87f80d510e08c39
RobertasTu <tumelis.robertas@gmail.com>
2021-08-13 13:19:41 +0300

    Pabaigtas trikampis
    Sutvarkyta , trikampio kampu skaiciavimas, bei keturkampio braizymas

--- kampai.php
+++ kampai.php
@@ -1,13 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Trikampis</title>
-</head>
-<body>
-<form action='trikampis.php' method='get'>
+
+
+<form action='kampai.php' method='get'>
     <input type='text' value='3' name='krastineA' />
     <input type='text' value='4' name='krastineB' />
     <input type='text' value='5' name='krastineC' />
@@ -17,18 +10,9 @@
 <div class='rezultatas'>
 
 </div>
-</body>
-</html>
-
 
 
 <?php 
-// 2. Susikurti klasę "Trikampis". Pagal klasę sukurti objektą.
-// Objektas turi priimti du kintamuosius: a, b, c kraštines.
-// Turi būti tikrinama ar trikampį galima sudaryti.
-// Sukurti metodus, kurie skaičiuoja plotą, perimetrą, pusperimetrį, bei kiekvieną iš trikampio kampų.
-// Informaciją išvesti į <p> žymę.
-
 
 class Trikampis {
     public $krastineA;
@@ -48,10 +32,10 @@ class Trikampis {
         $this->Perimetras();
         $this->Plotas();
         $this->pusPerimetris();
+        // $this-> trKampai()
         $this->kampasA();
         $this->kampasB();
         $this->kampasC();
-        
 
     }
     function arTrikampis() {
@@ -80,30 +64,32 @@ class Trikampis {
        echo '<br>';
     }
     function kampasA() {
-        $this->laikinaskampasA = acos(($this->krastineB*$this->krastineB+$this->krastineC*$this->krastineC-$this->krastineA*$this->krastineA)/(2*$this->krastineB*$this->krastineC));
-        $this->laikKampasA = (($this->laikinaskampasA * 180) / pi());
-         $this->rezultatas = round($this->laikKampasA,2);
-         echo $this->rezultatas.'=KampasA';
-         echo '<br>';
-     }
-     function kampasB(){
-         $this->laikinaskampasB = acos(($this->krastineA*$this->krastineA+$this->krastineC*$this->krastineC-$this->krastineB*$this->krastineB)/(2*$this->krastineA*$this->krastineC));
-         $this->laikKampasB = (($this->laikinaskampasB  * 180) / pi());
-         $this->rezultatas = round($this->laikKampasB,2);
-         echo $this->rezultatas.'=KampasB';
-         echo '<br>';
-     }
- 
-        
-     function kampasC(){
-         $this->laikinaskampasC = acos(($this->krastineA*$this->krastineA+$this->krastineB*$this->krastineB-$this->krastineC*$this->krastineC)/(2*$this->krastineA*$this->krastineB));
-         $this->laikKampasC = (($this->laikinaskampasC * 180) / pi());
-         $this->rezultatas = round($this->laikKampasC,2);
-         echo $this->rezultatas.'=KampasC';        
-         echo '<br>';
-     }
+       $this->laikinaskampasA = acos(($this->krastineB*$this->krastineB+$this->krastineC*$this->krastineC-$this->krastineA*$this->krastineA)/(2*$this->krastineB*$this->krastineC));
+       $this->laikKampasA = (($this->laikinaskampasA * 180) / pi());
+        $this->rezultatas = round($this->laikKampasA,2);
+        echo $this->rezultatas.'=KampasA';
+        echo '<br>';
+    }
+    function kampasB(){
+        $this->laikinaskampasB = acos(($this->krastineA*$this->krastineA+$this->krastineC*$this->krastineC-$this->krastineB*$this->krastineB)/(2*$this->krastineA*$this->krastineC));
+        $this->laikKampasB = (($this->laikinaskampasB  * 180) / pi());
+        $this->rezultatas = round($this->laikKampasB,2);
+        echo $this->rezultatas.'=KampasB';
+        echo '<br>';
+    }
 
-}
+       
+    function kampasC(){
+        $this->laikinaskampasC = acos(($this->krastineA*$this->krastineA+$this->krastineB*$this->krastineB-$this->krastineC*$this->krastineC)/(2*$this->krastineA*$this->krastineB));
+        $this->laikKampasC = (($this->laikinaskampasC * 180) / pi());
+        $this->rezultatas = round($this->laikKampasC,2);
+        echo $this->rezultatas.'=KampasC';        
+        echo '<br>';
+    }
+
+    }
+    
+
 if(isset($_GET['skaiciuoti'])) {
     if(isset($_GET['krastineA']) && !empty($_GET['krastineA']) && isset($_GET['krastineB']) && !empty($_GET['krastineB']) && isset($_GET['krastineC']) && !empty($_GET['krastineC'])) {
         $krastineA = $_GET['krastineA'];
@@ -114,7 +100,7 @@ if(isset($_GET['skaiciuoti'])) {
         if($krastineA + $krastineB > $krastineC && $krastineB + $krastineC > $krastineA && $krastineC + $krastineA > $krastineB) {
             echo 'Trikampi sudaryti galima';
             echo '<br>';
-            $trikampis = new Trikampis($krastineA, $krastineB, $krastineC);
+            $Trikampis = new Trikampis($krastineA, $krastineB, $krastineC);
         }
         else {
             echo 'Trikampio sudaryti negalima';
@@ -125,7 +111,6 @@ if(isset($_GET['skaiciuoti'])) {
         echo 'truksta kintamuju';
     } 
 } 
-    
 
 
 ?>
--- keturkampis.php
+++ keturkampis.php
@@ -1,24 +1,41 @@
+<!-- 3. Susikurti klasę "Keturkampis". Pagal klasę sukurti objektą.
+Objektas turi priimti du kintamuosius: a ir b kraštines.
+Sukurti metodus, kurie skaičiuoja kvadrato plotą, perimetrą, įstrižainės ilgį.
+Informaciją išvesti į <p> žymę.
+
+Papildoma: nubraižyti kvadratą(pikseliais) pagal a ir b kraštines. Kvadrato nubraižymas turi būti objekto metodas. --> -->
+
+
+
+<form action='keturkampis.php' method='get'>
+    <input type='text' value='' name='a' />
+    <input type='text' value='' name='b' />
+      <button type='submit' name='skaiciuoti'>Patvirtinti</button>
+
+</form>
+<div class='rezultatas'>
+
+</div>
 
 
 <?php
+
 class Keturkampis {
-
-public  $krastineA;
-
-public  $krastineB;
+public  $a;
+public  $b;
+public $rezultatas;
 
 
 
 
 
 function __construct($a,$b){
-
-    $this -> KrastineA = $a;
-
-    $this -> KrastineB = $b;
-
+    $this ->a = floatval($a);
+    $this ->b = floatval($b);
     $this -> perimetras();
-
+    $this-> plotas();
+    $this->istrizaine();
+    $this->braizymas();
 
 
 }
@@ -26,26 +43,54 @@ function __construct($a,$b){
 
 
 function perimetras (){
-
-
-
-$perimetras = (($this -> KrastineA + $this -> KrastineB)*2);
-
-//nei isvedi, nei returnini
-
-    echo $perimetras;
-
+$this->rezultatas = (($this->a+$this->b)*2);
+  echo 'Perimetras='.$this->rezultatas;
+   echo '<br>';
 
 
 }
 
+function plotas () {
+    $this->rezultatas = $this->a*$this->b;
+    echo 'Plotas='.$this->rezultatas;
+    echo '<br>';
+}
+
+function istrizaine() {
+    $this->rezultatas = sqrt($this->a**2 + $this->b**2);
+    echo 'Istrizaines ilgis ='.$this->rezultatas;
+    echo '<br>';
+}
+
+function braizymas()
+        {
+            echo '<br>Keturkampis:';
+            echo '<div class="braizymas" ';
+            echo 'style="height:'.$this->a.'px; width:'.$this->b.'px;';   
+            echo 'background:green" ></div>';
+
+        }
 
 
+
+}
+if(isset($_GET['skaiciuoti'])) {
+    if(isset($_GET['a']) && !empty($_GET['a']) && isset($_GET['b']) && !empty($_GET['b'])) {
+        $a = $_GET['a'];
+        $b = $_GET['b'];
+        
+        $rezultatas;
+
+
+        $keturkampis = new Keturkampis($a, $b);
+    } else {
+        echo 'truksta kintamuju';
+    } 
 }
 
 
 
-$perimetras = new Keturkampis (5,6);
+
 
 
 ?>
